14e7fd7899be99ed4390913736cbebec4d346879
Julia Shi <julia.shi@nyu.edu>
2020-08-03 10:14:14 +0800

    last one

--- string/create.rb
+++ string/create.rb
@@ -1,10 +1,37 @@
-def find_first_nonzero_among(numbers)
-  numbers.each do |n|
-    return n if n.nonzero?
-  end
-end
+x=0
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+  'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+  'flow', 'neon']
 
-# Examples
+new = []
+while x < words.length 
+  single = [words[x]]
+  y = 0
+  while y < words.length
+    if words[x].split("").sort == words[y].split("").sort
+    single << words[y] 
+    end  
+    y += 1
+  end 
+  new << single.uniq.sort
+  x += 1
+end 
 
-find_first_nonzero_among(0, 0, 1, 0, 2, 0)
-find_first_nonzero_among(1)
+p new.uniq
+=begin
+result = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else 
+    result[key]= [word]
+  end 
+end 
+result.each_value do |v|
+  puts "------"
+  p v
+end 
+
+=end 
